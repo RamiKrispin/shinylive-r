eba0fdd441744daa957c0665d9a68074a7e133f3
RamiKrispin <ramkrisp@umich.edu>
2023-09-22 03:55:53 -0700

    rename the site filder name

--- site/shinylive/webr/repl/components/Plot.d.ts
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import './Plot.css';
-import { PlotInterface } from '../App';
-export declare function Plot({ plotInterface, }: {
-    plotInterface: PlotInterface;
-}): React.JSX.Element;
-export default Plot;
--- site/shinylive/webr/webR/config.d.ts
+++
@@ -1,2 +0,0 @@
-export declare const BASE_URL: string;
-export declare const PKG_BASE_URL = "https://repo.r-wasm.org";
